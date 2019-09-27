a5c315841dfa92a5ac66cfef64c41ba970a29e8b
weimin96 <735313582@qq.com>
2019-09-27 22:44:36 +0800

    update

--- core/src/main/resources/static/admin/js/index.js
+++ core/src/main/resources/static/admin/js/index.js
@@ -1,11 +1,10 @@
 $(function(){
     //菜单点击
-    /*J_iframe
-    $(".J_menuItem").on('click',function(){
-        var url = $(this).attr('href');
-        $("#J_iframe").attr('src',url);
+    $(".iframe_item").on('click',function(){
+        var url = $(this).attr('data-url');
+        $("#iframe").attr('src',url);
         return false;
-    });*/
+    });
 
     $(".aside-select-li").on("click",function(){
         if($(this).hasClass("aside-select")){
@@ -13,10 +12,25 @@ $(function(){
         }else{
             $(this).addClass("aside-select");
         }
-    })
-    
+    });
+
+    let $container = $("#container");
     $(".menu-fold").on("click",function () {
-        
+        if($container.hasClass("aside-hover")) {
+            $container.removeClass("aside-hidden aside-hover");
+        }else{
+            $container.addClass("aside-hidden aside-hover");
+        }
+    });
+
+    $(".aside-list").hover(function(){
+        if($container.hasClass("aside-hover")){
+            $container.removeClass("aside-hidden");
+        }
+    },function () {
+        if($container.hasClass("aside-hover")) {
+            $container.addClass("aside-hidden");
+        }
     })
 });
 
