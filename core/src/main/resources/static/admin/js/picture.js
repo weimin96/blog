f2aab6075ac171580581939167a6767d842a0a8a
weimin96 <735313582@qq.com>
2019-10-19 20:16:38 +0800

    图片管理功能修改

--- core/src/main/resources/static/admin/js/picture.js
+++ core/src/main/resources/static/admin/js/picture.js
@@ -4,7 +4,11 @@ let vm;
 let app = new Vue({
     el: "#app",
     data: {
-
+        pageNum: 1,
+        records: [],
+        loading: false,
+        noMore: false,
+        id:-1
     },
     beforeCreate(){
         vm = this;
@@ -12,9 +16,28 @@ let app = new Vue({
     mounted(){
 
     },
+    computed: {
+        disabled () {
+            console.log("loading", this.loading);
+            console.log("noMore", this.noMore);
+            var isExit = $("#"+vm.id).length>0;
+            return this.loading || this.noMore;
+        }
+    },
     methods:{
         init(){
 
+        },
+        load(){
+            this.loading = true;
+            this.$http.get('/getImageList', {params: {"pageNum": this.pageNum,"pageSize": 10}}).then(function (response) {
+                var records = response.data.data.records;
+                vm.records = vm.records.concat(records);
+                vm.id = vm.records[vm.records.length-1].id;
+                vm.noMore = vm.records.length >= response.data.data.total;
+                vm.loading = false;
+                vm.pageNum++;
+            })
         }
     }
 });
