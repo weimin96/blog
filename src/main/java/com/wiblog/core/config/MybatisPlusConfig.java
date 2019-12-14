8dcb5d9ae451ff105c3a047e2664a25ce5fb2f4b
weimin96 <735313582@qq.com>
2019-12-14 17:15:14 +0800

    用户注册bug解决

--- src/main/java/com/wiblog/core/config/MybatisPlusConfig.java
+++ src/main/java/com/wiblog/core/config/MybatisPlusConfig.java
@@ -3,10 +3,10 @@ package com.wiblog.core.config;
 
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
-
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 /**
  * MybatisPlus配置
@@ -30,7 +30,7 @@ public class MybatisPlusConfig {
      * SQL执行效率插件
      */
     @Bean
-//    @Profile({"dev","test"})// 设置 dev test 环境开启
+    @Profile("dev")// 设置 dev  环境开启
     public PerformanceInterceptor performanceInterceptor() {
         return new PerformanceInterceptor();
     }
