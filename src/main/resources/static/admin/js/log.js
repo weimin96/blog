ce089d3615d99c496d8b71cdc3118deef34e90b2
weimin96 <735313582@qq.com>
2020-03-13 00:14:19 +0800

    日志监控

--- src/main/resources/static/admin/js/log.js
+++ src/main/resources/static/admin/js/log.js
@@ -16,8 +16,8 @@ let app = new Vue({
     methods: {
         createWebsocket(){
             let token = this.$cookies.get("uToken");
-            // this.websocket = new WebSocket('wss://www.wiblog.cn/websocket/log');
-            this.websocket = new WebSocket('wss://127.0.0.1/websocket/log/'+token);
+            this.websocket = new WebSocket('wss://www.wiblog.cn/websocket/log');
+            // this.websocket = new WebSocket('wss://127.0.0.1/websocket/log/'+token);
             this.websocket.onopen = this.open;
             this.websocket.onmessage = this.onmessage;
         },
