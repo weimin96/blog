cde6c91e9482e018ecea9f9bab0909d9ef7054f8
weimin96 <735313582@qq.com>
2020-05-04 12:22:22 +0800

    微信公众号

--- src/main/java/com/wiblog/core/utils/MessageUtil.java
+++ src/main/java/com/wiblog/core/utils/MessageUtil.java
@@ -3,11 +3,11 @@ package com.wiblog.core.utils;
 
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.DomDriver;
+import com.wiblog.core.weixin.TextMessage;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
-import org.springframework.web.socket.TextMessage;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
@@ -21,103 +21,6 @@ import java.util.Map;
  * @date 2020/4/3
  */
 public class MessageUtil {
-
-    /**
-     * 返回消息类型：文本
-     */
-    public static final String RESP_MESSAGE_TYPE_TEXT = "text";
-
-    /**
-     * 返回消息类型：音乐
-     */
-    public static final String RESP_MESSAGE_TYPE_MUSIC = "music";
-
-    /**
-     * 返回消息类型：图文
-     */
-    public static final String RESP_MESSAGE_TYPE_NEWS = "news";
-
-    /**
-     * 返回消息类型：图片
-     */
-    public static final String RESP_MESSAGE_TYPE_IMAGE = "image";
-
-    /**
-     * 返回消息类型：语音
-     */
-    public static final String RESP_MESSAGE_TYPE_VOICE = "voice";
-
-    /**
-     * 返回消息类型：视频
-     */
-    public static final String RESP_MESSAGE_TYPE_VIDEO = "video";
-
-    /**
-     * 请求消息类型：文本
-     */
-    public static final String REQ_MESSAGE_TYPE_TEXT = "text";
-
-    /**
-     * 请求消息类型：图片
-     */
-    public static final String REQ_MESSAGE_TYPE_IMAGE = "image";
-
-    /**
-     * 请求消息类型：链接
-     */
-    public static final String REQ_MESSAGE_TYPE_LINK = "link";
-
-    /**
-     * 请求消息类型：地理位置
-     */
-    public static final String REQ_MESSAGE_TYPE_LOCATION = "location";
-
-    /**
-     * 请求消息类型：音频
-     */
-    public static final String REQ_MESSAGE_TYPE_VOICE = "voice";
-
-    /**
-     * 请求消息类型：视频
-     */
-    public static final String REQ_MESSAGE_TYPE_VIDEO = "video";
-
-    /**
-     * 请求消息类型：推送
-     */
-    public static final String REQ_MESSAGE_TYPE_EVENT = "event";
-
-    /**
-     * 事件类型：subscribe(订阅)
-     */
-    public static final String EVENT_TYPE_SUBSCRIBE = "subscribe";
-
-    /**
-     * 事件类型：unsubscribe(取消订阅)
-     */
-    public static final String EVENT_TYPE_UNSUBSCRIBE = "unsubscribe";
-
-    /**
-     * 事件类型：CLICK(自定义菜单点击事件)
-     */
-    public static final String EVENT_TYPE_CLICK = "CLICK";
-
-    /**
-     * 事件类型：VIEW(自定义菜单URl视图)
-     */
-    public static final String EVENT_TYPE_VIEW = "VIEW";
-
-    /**
-     * 事件类型：LOCATION(上报地理位置事件)
-     */
-    public static final String EVENT_TYPE_LOCATION = "LOCATION";
-
-    /**
-     * 事件类型：LOCATION(上报地理位置事件)
-     */
-    public static final String EVENT_TYPE_SCAN = "SCAN";
-
-
 
     /**
      * xml转换为map
--- src/main/java/com/wiblog/core/weixin/WeixinUtil.java
+++ src/main/java/com/wiblog/core/weixin/WeixinUtil.java
@@ -2,6 +2,7 @@ package com.wiblog.core.weixin;
 
 import com.alibaba.fastjson.JSONObject;
 import com.wiblog.core.utils.MessageUtil;
+import com.wiblog.core.utils.VerifyCodeUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.dom4j.Document;
@@ -45,11 +46,12 @@ public class WeixinUtil {
 
     /**
      * 获取accessToken
+     *
      * @return accessToken
      */
-    public String getAccessToken(){
+    public String getAccessToken() {
         String accessToken = (String) redisTemplate.opsForValue().get(WeixinConstant.ACCESS_TOKEN_KEY);
-        if (StringUtils.isBlank(accessToken)){
+        if (StringUtils.isBlank(accessToken)) {
             return setAccessToken();
         }
         return accessToken;
@@ -57,9 +59,10 @@ public class WeixinUtil {
 
     /**
      * 设置accessToken
+     *
      * @return accessToken
      */
-    public String setAccessToken(){
+    public String setAccessToken() {
         /*准备发送GET请求*/
         String url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=APPSECRET";
         url = url.replace("APPID", appId).replace("APPSECRET", appSecret);
@@ -67,17 +70,18 @@ public class WeixinUtil {
         String response = restTemplate.getForObject(url, String.class);
         String accessToken = (String) JSONObject.parseObject(response).get("access_token");
 
-        if (StringUtils.isBlank(accessToken)){
-            log.error("设置AccessToken失败：appid：{},appSecret：{}",appId,appSecret);
+        if (StringUtils.isBlank(accessToken)) {
+            log.error("设置AccessToken失败：appid：{},appSecret：{}", appId, appSecret);
             return null;
         }
-        redisTemplate.opsForValue().set(WeixinConstant.ACCESS_TOKEN_KEY,accessToken,7200, TimeUnit.SECONDS);
-        log.info("设置accessToken->{}",accessToken);
+        redisTemplate.opsForValue().set(WeixinConstant.ACCESS_TOKEN_KEY, accessToken, 7200, TimeUnit.SECONDS);
+        log.info("设置accessToken->{}", accessToken);
         return accessToken;
     }
 
     /**
      * 获取ticket
+     *
      * @return ticket
      */
     public String getTicket() {
@@ -86,6 +90,7 @@ public class WeixinUtil {
 
     /**
      * 设置ticket
+     *
      * @param accessToken accessToken
      * @return ticket
      */
@@ -98,11 +103,11 @@ public class WeixinUtil {
         String errcode = jsonObject.getString("errcode");
         if ("0".equals(errcode)) {
             String ticket = jsonObject.getString("ticket");
-            redisTemplate.opsForValue().set(WeixinConstant.JS_API_TICKET_KEY,ticket,7200, TimeUnit.SECONDS);
-            log.info("设置ticket成功：{}",ticket);
+            redisTemplate.opsForValue().set(WeixinConstant.JS_API_TICKET_KEY, ticket, 7200, TimeUnit.SECONDS);
+            log.info("设置ticket成功：{}", ticket);
             return ticket;
         }
-        log.error("设置ticket失败,errcode：{}",errcode);
+        log.error("设置ticket失败,errcode：{}", errcode);
         return null;
     }
 
@@ -174,7 +179,7 @@ public class WeixinUtil {
         List<Element> elementList = root.elements();
 
         // 遍历所有子节点
-        for (Element e : elementList){
+        for (Element e : elementList) {
             map.put(e.getName(), e.getText());
         }
 
@@ -190,7 +195,7 @@ public class WeixinUtil {
         try {
             // xml请求解析
             Map<String, String> requestMap = MessageUtil.xmlToMap(request);
-            if (requestMap == null){
+            if (requestMap == null) {
                 return respMessage;
             }
             // 发送方帐号（openid）
@@ -199,11 +204,45 @@ public class WeixinUtil {
             String toUserName = requestMap.get("ToUserName");
             // 消息类型
             String msgType = requestMap.get("MsgType");
+            // 事件类型
+            String eventType = requestMap.get("Event");
             // 消息内容
             String content = requestMap.get("Content");
             log.info("接收微信消息{}", requestMap);
-        }catch (Exception e){
-            log.error(e.getMessage(),e);
+
+            MsgEnum msgEnum = MsgEnum.fromString(msgType);
+            TextMessage text = new TextMessage();
+            // 事件
+            if (msgEnum == MsgEnum.EVENT) {
+                //扫码关注公众号
+                //{CreateTime=1588561340, EventKey=, Event=subscribe,
+                // ToUserName=gh_27c767c19e20, FromUserName=oCtGTwMjE3zmLU6uIuJIvnv6-UKs, MsgType=event}
+                EventEnum eventEnum = EventEnum.fromString(eventType);
+                // 订阅
+                if (eventEnum == EventEnum.SUBSCRIBE) {
+
+                    StringBuilder sb = new StringBuilder();
+                    sb.append("Hi，欢迎关注歪博~").append("\n\n");
+                    sb.append("点击获取网站登录校验码").append("\n");
+                    sb.append("<a href=\"weixin://bizmsgmenu?msgmenucontent=登录&msgmenuid=1\">登录</a>");
+                    text.setContent(sb.toString());
+                }
+            }else if(msgEnum == MsgEnum.TEXT){
+                //接收回复
+                //{Content=登录, CreateTime=1588565004, ToUserName=gh_27c767c19e20,
+                // FromUserName=oCtGTwMjE3zmLU6uIuJIvnv6-UKs, MsgType=text, MsgId=22742783737763533}
+                String code = VerifyCodeUtils.getRandomCode();
+                text.setContent("验证码："+code+"/n/n本验证码5分钟内有效，如超时请重新发送“登录”二字获取");
+            }
+
+            text.setToUserName(fromUserName);
+            text.setFromUserName(toUserName);
+            text.setCreateTime(String.valueOf(System.currentTimeMillis()));
+            text.setMsgType("text");
+            respMessage = MessageUtil.textMessageToXml(text);
+
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
         }
         return respMessage;
     }
